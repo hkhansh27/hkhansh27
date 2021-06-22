5d070290faa9cbf3329130c138640a55cf12b1ea
Huỳnh Hữu Khánh <HKhansh@HKhansh-2.local>
2021-06-22 11:49:55 +0700

    Update

--- .history/index_20210306144641.js
+++ .history/index_20210306144641.js
@@ -24,7 +24,7 @@ const generate = async () => {
 
   if (!quote) return;
 
-  fs.writeFileSync("README.md", `> ${quote} *${author}*`);
+  fs.writeFileSync("README.md", `_**${quote}**_\n\n${author}`);
 };
 
 generate();
